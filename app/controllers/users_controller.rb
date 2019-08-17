f4ec042a3152ab5a8fe1e4041d7e4b2702830cde
s.tomoki <tanajammw@gmail.com>
2019-08-18 00:59:51 +0900

    登録と更新を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,27 +8,35 @@ class UsersController < ApplicationController
   end
 
   def new
-    
+    @user = User.new
   end
 
   def new_confirm
-
+    @user = User.new(user_params)
+    render :new unless @user.valid?
   end
 
   def create
-
+    @user = User.new(user_params)
+    render :new unless @user.save
+    redirect_to users_path, flash: {success: '登録しました'}
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def edit_confirm
-
+    @user = User.find(params[:id])
+    @user.attributes = user_params
+    render :new unless @user.valid?
   end
 
   def update
-
+    @user = User.find(params[:id])
+    @user.attributes = user_params
+    render :edit unless @user.save
+    redirect_to users_path, flash: {success: '更新しました'}
   end
 
   def destroy
@@ -41,5 +49,11 @@ class UsersController < ApplicationController
   def import
     User.bulk_insert(params[:file])
     redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :name, :address, :email)
   end
 end
