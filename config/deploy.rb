e93301a225fab8f660b9a372bc098df6dfb63113
s.tomoki <tanajammw@gmail.com>
2019-08-20 01:52:51 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,28 +20,6 @@ namespace :deploy do
       execute "sudo service nginx start"
     end
   end
-  
-  desc 'Upload database.yml'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/database.yml', "#{shared_path}/config/database.yml")
-    end
-  end
 
-  desc 'Create Database'
-  task :db_create do
-    on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:create'
-      end
-      end
-    end
-  end
-
-  before :starting, :upload
   after  :finished, :nginx_restart
 end
